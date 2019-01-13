f1a245771809ae3b3041ae454163674d06ee2d5e
Maruša <marusa.konecnik@student.um.si>
2019-01-13 18:32:34 +0100

    added map with current location of a user.

--- app/src/main/java/com/example/mercenex/speedingticket/Camera.java
+++ app/src/main/java/com/example/mercenex/speedingticket/Camera.java
@@ -2,24 +2,45 @@ package com.example.mercenex.speedingticket;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.GpsStatus;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import static android.media.MediaScannerConnection.scanFile;
 
 public class Camera extends AppCompatActivity {
 
     ImageView imageView;
+    private int CAMERA_CAPTURE_IMAGE_REQUEST_CODE;
+    private Button mapButton = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
+        mapButton = findViewById(R.id.btnGoToMap);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
        Button btnCamera = findViewById(R.id.btnCamera);
-        ImageView imageView = findViewById(R.id.cameraView);
+       ImageView imageView = findViewById(R.id.cameraView);
 
         btnCamera.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -30,11 +51,14 @@ public class Camera extends AppCompatActivity {
         });
     }
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
         ImageView image = findViewById(R.id.cameraView);
         image.setImageBitmap(bitmap);
+
     }
+
+
 
 }
